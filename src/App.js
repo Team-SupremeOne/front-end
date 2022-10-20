76fee7d1137ef84ffdf86c52038982bbf043c609
thizzy824 <agpastora7@gmail.com>
2022-10-20 15:57:33 -0700

    deleted unnecessary files/logo, refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes, Link } from 'react-router-dom';
+import Nav from './components/Nav';
 import './App.css';
+import ArtWork from './components/ArtWork';
+import ArtDetails from './components/ArtDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<header>
+				<Link to='/'>
+					<h1 className='title'>Supreme Art Gallery</h1>
+				</Link>
+				<Nav />
+			</header> 
+      <h2 className='title'>Welcome to Supreme Art Gallery</h2>
+			<main>
+				<Routes>
+					<Route path='/' element={<ArtWork />} />
+					<Route path='details/:id' element={<ArtDetails />} />
+				</Routes>
+			</main>
+		</>
+	);
 }
 
 export default App;
