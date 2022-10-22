861d7faca1a55dedea973f79768d22a9ffac4c30
thizzy824 <agpastora7@gmail.com>
2022-10-22 08:46:50 -0700

    create buttons and functions to edit/delete art

--- src/components/ArtDetails.js
+++ src/components/ArtDetails.js
@@ -1,15 +1,38 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function ArtDetails() {
 	let { id } = useParams();
 	const [art, setArt] = useState([]);
+	const [modal, setModal] = useState(false);
 
 	useEffect(() => {
 		fetch(`http://localhost:6060/artworks/${id}`)
 			.then((response) => response.json())
 			.then((data) => setArt(data));
 	}, []);
+
+	const editShowPage = () => {
+		setModal(true);
+	};
+
+	const closeModal = () => {
+		setModal(false);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Write your PUT fetch() or axios() request here
+		axios.put(`http://localhost:6060/artworks/${id}`, art);
+		// navigate('/');
+	};
+
+	const handleDelete = () => {
+		// Write your DELETE fetch() or axios() request here
+		axios.delete(`http://localhost:6060/artworks/${id}`, art);
+		// navigate('/');
+	};
 
 	if (!art) {
 		return <h2 className='loading'>Loading...</h2>;
@@ -24,6 +47,8 @@ function ArtDetails() {
 				<p>{art.description}</p>
 				<h3>More ArtWork from this Artist</h3>
 				<h4>{art.artistInfo}</h4>
+				<button onClick={editShowPage}>Edit</button>
+				<button onClick={handleDelete}>Delete</button>
 			</div>
 		</div>
 	);
