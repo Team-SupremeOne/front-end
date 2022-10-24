43cf6b56392f5eee396da634ea4fe671b7a342b0
thizzy824 <agpastora7@gmail.com>
2022-10-24 15:27:53 -0700

    add aboutUs path/route

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 function AboutUs() {
-    return (
-        <div>
-            <h1>About Us</h1>
-            
-        </div>
-    );
+	return (
+		<div>
+			<h1 className='add-title'>About Us</h1>
+		</div>
+	);
 }
 
 export default AboutUs;
